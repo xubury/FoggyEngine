7858e8d29bf1e834d1bb9dff3b646cde6d4e2648
xubury <xubury97@gmail.com>
2020-11-14 19:05:59 +0800

    * add unit convertor

---
+++ include/util/converter.hpp
@@ -0,0 +1,29 @@
+namespace foggy {
+namespace converter {
+
+constexpr double PIXELS_PER_METERS = 32.0;
+
+constexpr double PI = 3.14159265358979323846;
+
+template <typename T>
+inline constexpr T PixelsToMeters(const T& x) {
+    return x / PIXELS_PER_METERS;
+}
+
+template <typename T>
+inline constexpr T MetersToPixels(const T& x) {
+    return x * PIXELS_PER_METERS;
+}
+
+template <typename T>
+inline constexpr T DegToRad(const T& x) {
+    return x * PI / 180;
+}
+
+template <typename T>
+inline constexpr T RadToDeg(const T& x) {
+    return x * 180 / PI;
+}
+
+} /* namespace converter */
+} /* namespace foggy */
