7fe03614be4ea53a545b51d8eae95a1bffe6fa1e
xubury <xubury97@gmail.com>
2020-11-16 14:17:02 +0800

    * make outline thickness to 1 pixel

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -6,7 +6,7 @@ Entity::Entity(ShapePtr shape, b2BodyType type)
     : m_b2_type(type), m_shape(std::move(shape)) {
     m_shape->setFillColor(sf::Color::Transparent);
     m_shape->setOutlineColor(sf::Color::Green);
-    m_shape->setOutlineThickness(2.f);
+    m_shape->setOutlineThickness(1.f);
 }
 
 }  // namespace foggy
